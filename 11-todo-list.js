de278b76ef25b5f562fd5ed61d98b627f41a8699
Amit Jape <japeamit@gmail.com>
2025-06-27 16:48:06 +0530

    Todo List JS

---
+++ 11-todo-list.js
@@ -0,0 +1,39 @@
+const todoList = [];
+ 
+renderTodoList();
+
+function renderTodoList() {
+            let todoListHTML = '';
+
+
+            // to generate the HTML for each todo item in the todoList array
+            // we will use a for loop to iterate through the todoList array
+            // and for each todo item, we will create a paragraph element with the todo item text
+            // and add it to the todoListHTML string.
+            // finally, we will set the innerHTML of the todoList element to the todoListHTML string.
+            // this will render the todo list on the page.
+            // console.log(todoList);
+            for(let i=0; i<todoList.length; i++) {
+              const todo = todoList[i];
+              const html = `<p>${todo} <button> Delete </button> </p>`
+              todoListHTML += html;
+
+            }
+            console.log(todoListHTML);
+
+
+            document.querySelector('.js-todo-list').innerHTML = todoListHTML; // putting the todolistHTML array of lists, into the HTML div element.
+            console.log(todoListHTML);
+}
+
+function addTodo() {
+          const inputElement = document.querySelector('.js-name-input');
+          const name = inputElement.value;
+          //  console.log(name);
+          todoList.push(name);
+          console.log(todoList);
+
+            inputElement.value = '';
+            renderTodoList();
+        }
+
